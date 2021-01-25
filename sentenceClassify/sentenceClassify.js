e92be5f9b07bca3480ab42bb50372182b0b0c0ac
JooJaeSeong <joojae26@gmail.com>
2021-01-25 23:43:03 +0900

    Data for model training

--- sentenceClassify/sentenceClassify.js
+++ sentenceClassify/sentenceClassify.js
@@ -13,9 +13,10 @@ const idToLabels = { 0: "부정", 1: "긍정" };
 
 //각 문장 띄어쓰기 단위로 분할
 let words = [];
-sentences.forEach((x) => {
-  words.push(...x.split(" "));
+sentences.forEach((sentence) => {
+  words.push(...sentence.split(" "));
 });
+
 //중복 단어 제거
 words = [...new Set(words)];
 //각 단어별 고유한 번호 부여
@@ -24,5 +25,34 @@ wordToId = words.reduce((acc, cur, i) => {
   acc[cur] = i + 2;
   return acc;
 }, wordToId);
-// 각 숫자별 단어 부여
-wordToId = Object.fromEntries(Object.entries(wordToId).map((x) => x.reverse()));
+//각 숫자별 단어 부여
+const idToWord = Object.fromEntries(
+  Object.entries(wordToId).map((x) => x.reverse())
+);
+
+// Data for model training
+
+//학습용 입력 데이터 생성
+let trainInputs = [];
+const getKeyByValue = (obj, value) =>
+  Object.keys(obj).find((key) => obj[key] === value);
+
+sentences.forEach((sentence) => {
+  const keys = [];
+  [...sentence.split(" ")].forEach((word) => {
+    keys.push(parseInt(getKeyByValue(idToWord, word)));
+  });
+  trainInputs.push(keys);
+});
+//문장의 길이를 모두 동일하게 변경 (최대길이 4)
+trainInputs = trainInputs.map((array) => {
+  const length = array.length;
+  array.length = 4;
+  return array.fill(0, length);
+});
+//train label은 label을 그대로 사용
+let trainLabels = labels;
+//train inputs을 2D 텐서로 변환
+trainInputs = tf.tensor2d(trainInputs, [2, 4]).print();
+//train label 학습용 정답을 2D 텐서로 변환
+trainLabels = tf.tensor2d(trainLabels, [1, 2]).print();
