b303dd297b2ddf823a6ff976913ea25da866451b
JooJaeSeong <joojae26@gmail.com>
2021-01-26 03:32:20 +0900

    Build Model & Get Model

--- sentenceClassify/sentenceClassify.js
+++ sentenceClassify/sentenceClassify.js
@@ -1,4 +1,5 @@
 import * as tf from "@tensorflow/tfjs";
+import { logSigmoid } from "@tensorflow/tfjs";
 
 // Data
 
@@ -53,6 +54,73 @@ trainInputs = trainInputs.map((array) => {
 //train label은 label을 그대로 사용
 let trainLabels = labels;
 //train inputs을 2D 텐서로 변환
-trainInputs = tf.tensor2d(trainInputs, [2, 4]).print();
+trainInputs = tf.tensor2d(trainInputs, [2, 4]);
 //train label 학습용 정답을 2D 텐서로 변환
-trainLabels = tf.tensor2d(trainLabels, [1, 2]).print();
+trainLabels = tf.tensor1d(trainLabels);
+
+// Modelling
+
+//입력 단어를 vector로 변환
+const embedding = tf.layers.embedding({
+  inputDim: Object.keys(wordToId).length,
+  outputDim: 8,
+});
+const hidden = embedding.apply(trainInputs);
+hidden.print();
+//각 단어 벡터의 최대값 기준으로 벡터를 더해서 차원을 줄여줌 (문장 vector 생성)
+const pool = tf.layers.globalMaxPool1d();
+const hiddenPool = pool.apply(hidden);
+hiddenPool.print();
+//문장 vector를 이용해서 긍정(1), 부정(0) 확률값 예측
+const linear = tf.layers.dense({ units: 2, activation: "softmax" });
+const outputs = linear.apply(hiddenPool);
+outputs.print();
+//모델 만들기
+const buildModel = (numVocab, denseModel, numSeq, numOutput) => {
+  /* 
+  numVocab : vocabulary 단어 수
+  denseModel : 단어 의미하는 벡터 차원 수
+  numSeq : 문장길이
+  numOutput : 예측할 class 개수
+  */
+  const inputs = tf.input({ shape: [numSeq] });
+
+  const embedding = tf.layers.embedding({
+    inputDim: numVocab,
+    outputDim: denseModel,
+  });
+  const hidden = embedding.apply(inputs);
+  const pool = tf.layers.globalMaxPool1d();
+  const hiddenPool = pool.apply(hidden);
+  const linear = tf.layers.dense({ units: numOutput, activation: "softmax" });
+  const outputs = linear.apply(hiddenPool);
+
+  const model = tf.model({ inputs, outputs });
+  return model;
+};
+//모델 생성
+const model = buildModel(Object.keys(wordToId).length, 8, 4, 2);
+
+//학습
+//모델 loss, optimizer, metric 정의
+model.compile({
+  optimizer: "adam",
+  loss: "sparseCategoricalCrossentropy",
+  metrics: ["acc"],
+});
+//모델 학습
+const history = await model.fit(trainInputs, trainLabels, {
+  batchSize: 16,
+  epochs: 20,
+  callbacks: {
+    onEpochEnd: (epoch, logs) =>
+      console.log(
+        "Loss after Epoch " +
+          epoch +
+          " loss : " +
+          logs.loss +
+          " accuracy : " +
+          logs.acc
+      ),
+  },
+});
